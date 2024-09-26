f2ce5b33ea6a8f15bc3b1d91222a7bdd69504155
keshavjha205 <keshavjha1081@gmail.com>
2024-09-26 19:34:54 +0530

    c program

---
+++ c_programming.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+   int num1 = 1;
+   float decNum = 2.5;
+   char c = 'K';
+
+   printf("The value of number is %d\n", num1);
+   printf("The value of number is %f\n", decNum);
+   printf("The value is %c\n", c);
+
+    return 0;
+}
